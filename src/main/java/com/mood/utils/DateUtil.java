974c6075e7bc52bb19fdd80df6bd532325c33c41
wangzhifei <wangzhifei@renrenche.com>
2019-03-19 11:45:10 +0800

    Angel 增加解析ES返回结果方法 增加时间处理

--- src/main/java/com/mood/utils/DateUtil.java
+++ src/main/java/com/mood/utils/DateUtil.java
@@ -19,6 +19,8 @@ public class DateUtil {
 
     public static String defaultDateTimeFormatter="yyyy-MM-dd HH:mm:ss";
 
+    public static String TIME_FORMAT_LONG="yyyyMMddHHmmss";
+
     /**
      *
      * @param dateTimeFormatter
@@ -102,6 +104,20 @@ public class DateUtil {
             dateTimeFormatter=DateUtil.defaultDateTimeFormatter;
         }
         return  LocalDateTime.ofInstant(source.toInstant(), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern(dateTimeFormatter));
+    }
+    /**
+     *
+     * @param dateTimeFormatter
+     * @author: by Mood
+     * @date: 2011-01-14 11:11:11
+     * @Description: 线程安全转换时间
+     * @version: 1.0
+     */
+    public static Date convertDate(String source,String dateTimeFormatter){
+        if (dateTimeFormatter==null||dateTimeFormatter.equals("")){
+            dateTimeFormatter=DateUtil.defaultDateTimeFormatter;
+        }
+        return  Date.from(LocalDateTime.parse(source,DateTimeFormatter.ofPattern(dateTimeFormatter)).atZone(ZoneId.systemDefault()).toInstant());
     }
     /**
      *
--- src/main/java/com/mood/utils/ElasticSearchHandle.java
+++ src/main/java/com/mood/utils/ElasticSearchHandle.java
@@ -1,13 +1,20 @@
 package com.mood.utils;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 /**
  * @author: by Mood
@@ -97,6 +104,76 @@ public class ElasticSearchHandle {
         sb.append(ret);
         logger.info(sb.toString());
         return ret;
+    }
+    /**
+     *
+     * @param ret  ES返回结果
+     * @description: 解析结果
+     * @return
+     * @throws IOException
+     */
+    public static Map<String,Object> analysisResultMap(JSONObject ret){
+        Map<String,Object> map= Maps.newHashMap();
+        List<JSONObject> list= Lists.newLinkedList();
+        JSONObject hits= ret.getJSONObject("hits");
+        if(hits!=null){
+            int l=hits.getIntValue("total");
+            if(l>0){
+                JSONArray hitArr=hits.getJSONArray("hits");
+                for(int i=0;i<hitArr.size();i++){
+                    JSONObject hit=hitArr.getJSONObject(i).getJSONObject("_source");
+                    JSONObject result=function.apply(hit);
+                    list.add(result);
+                }
+                map.put("count",l);
+                map.put("data",list);
+            }
+        }
+        return map;
+    }
+    static Function<JSONObject,JSONObject> function= x->{
+        JSONObject result=x;
+        x.forEach((k,v)->{
+            //转换时间
+            if (k.contains("Time")){
+                if (v!=null&&!v.equals("")&&v.toString().length()==14){
+                    try {
+                        Date time= DateUtil.convertDate(v.toString(), DateUtil.TIME_FORMAT_LONG);
+                        result.put(k,DateUtil.dateFormat(time,DateUtil.defaultDateTimeFormatter));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    result.put(k,"");
+                }
+            }else {
+                result.put(k,(v!=null&&!v.equals(""))?v:"");
+            }
+        });
+        return result;
+    };
+    /**
+     *
+     * @param ret  ES返回结果
+     * @description: 解析结果
+     * @return
+     * @throws IOException
+     */
+    public static Map<String,JSONObject> analysisResultType(JSONObject ret){
+        Map<String,JSONObject> map= Maps.newHashMap();
+        JSONObject hits= ret.getJSONObject("hits");
+        if(hits!=null){
+            int l=hits.getIntValue("total");
+            if(l>0){
+                JSONArray hitArr=hits.getJSONArray("hits");
+                for(int i=0;i<hitArr.size();i++){
+                    JSONObject hit=hitArr.getJSONObject(i).getJSONObject("_source");
+                    JSONObject result=function.apply(hit);
+                    map.put("data",result);
+                }
+            }
+        }
+        return map;
     }
     public static void main(String args[])throws Exception{
         //must参数
