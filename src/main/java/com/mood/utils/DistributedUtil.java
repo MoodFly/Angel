3675600365d2400428aa07c68423ea6c0df05f9f
wangzhifei <wangzhifei@renrenche.com>
2019-05-22 14:30:59 +0800

    Angel 修改DistributedUtil 重构返回值校验

--- src/main/java/com/mood/utils/DistributedUtil.java
+++ src/main/java/com/mood/utils/DistributedUtil.java
@@ -8,18 +8,21 @@ import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author: by Mood
- * @date: 2019-04-24 10:38:14
+ * @date: 2019-05-22 14:19:27
  * @Description: 分布式锁操作工具类 支持 redis + Zookeeper
  * @version: 1.0
  */
 public class DistributedUtil {
 
     private final static Logger logger = LoggerFactory.getLogger(DistributedUtil.class);
+    private final static String OK="OK";
     private volatile static DistributedUtil distributedUtil=null;
     private DistributedUtil(){}
+    private Predicate<Object> predicate=s-> s != null && !StringUtils.isEmpty(s.toString()) && OK.equals(s.toString());
 
     /**
      * 获取分布式锁工具类调用实例
@@ -38,19 +41,13 @@ public class DistributedUtil {
      * @Description: 获取锁
      * @version: 1.0
      */
-    private boolean distributedRedisLock(Jedis jedis,String key, String value, Integer expire){
+    private boolean distributedRedisLock(Jedis jedis, String key, String value, Integer expire){
         if (jedis == null) {
             logger.error("get resource failed");
             return false;
         }
         Object s = operation(jedis,setnx(key, value, expire));
-        if(s == null || StringUtils.isEmpty(s.toString())){
-            return false;
-        }else if(s.toString().equals("OK")){
-            return true;
-        }else{
-            return false;
-        }
+        return predicate.test(s);
     }
     /**
      *
@@ -59,21 +56,16 @@ public class DistributedUtil {
      * @Description: 释放锁
      * @version: 1.0
      */
-    private boolean distributedRedisUnLock(Jedis jedis,String key){
+    private boolean distributedRedisUnLock(Jedis jedis, String key){
         Object s = operation(jedis,(x) -> x.del(key));
-        if(s == null || StringUtils.isEmpty(s.toString())){
-            return false;
-        }else if(s.toString().equals("OK")){
-            return true;
-        }else{
-            return false;
-        }
+        return predicate.test(s);
     }
-    private  Object operation(Jedis jedis,Function<Jedis, Object> f) {
-        if (f == null) return null;
+    private  Object operation(Jedis jedis, Function<Jedis, Object> f) {
+        if (f == null) {
+            return null;
+        }
         try {
-            Object o = f.apply(jedis);
-            return o;
+            return f.apply(jedis);
         } catch (Exception e) {
             logger.error("redis operation failed", e);
             return null;
@@ -82,8 +74,10 @@ public class DistributedUtil {
         }
     }
     private  Function<Jedis, Object> setnx(String key, String value, Integer expire) {
-        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value) || expire == null)
+        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value) || expire == null){
             throw new AngelException(ApplicationCode.unParamException);
+        }
         return (Jedis jedis) -> jedis.set(key, value, "nx", "ex", expire);
     }
 }
+
